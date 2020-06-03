16368b6205446341637e934e418de1500a8113d6
koshaapatel <koshapatel1045@gmail.com>
2020-06-03 04:51:50 -0400

    keyword example, let construct

--- main.clj
+++ main.clj
@@ -1,12 +1,20 @@
 
+;
+;(let [readinput (read-line)]
+;
+;     (if (= readinput "1" | = readinput "2")
+;       (println "i1")
+;       (println "Wrong")))
 
 
+(println (let [alpha 7] (* alpha 5)))
 
+(keyword 'by-nines)
+(keyword 'from)
+(keyword 'to)
+(defn capture
+      [by-nines from x to y]
+      ( filter (fn [x] (zero? (mod x 9))) (range x y))
+      )
 
-
-
-(let [readinput (read-line)]
-
-     (if (= readinput "1" | = readinput "2")
-       (println "i1")
-       (println "Wrong")))
+(println (capture :by-nines :from 100 :to 150))
