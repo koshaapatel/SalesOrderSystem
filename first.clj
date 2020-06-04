69c0e04fb9e724261dec3f459acdaf16ebbc1a6a
koshaapatel <koshapatel1045@gmail.com>
2020-06-04 01:52:18 -0400

    recursion completed

--- first.clj
+++ first.clj
@@ -1,54 +1,34 @@
-(defn func // gem
+(defn parse-int [s]
+   (Integer. (re-find  #"\d+" s )))
+
+(defn printmenu
   []
-      (println "wowo")
-      (let [readinput (read-line)]
-           (if (= readinput "1")
-             (println "i1")
-             (println "Wrong")))
+    (println "*** Sales Menu ***")
+    (println "------------------")
+    (println "1. Display Customer Table")
+    (println "2. Display Product Table")
+    (println "3. Display Sales Table")
+    (println "4. Total Sales for Customer")
+    (println "5. Total Count for Product")
+    (println "6. Exit")
+    (println "Enter an option?")
+)
 
-      (println "func calling")
-      (func)
+(defn takeinput 
+  []
+      (printmenu)
+      (let [readinput (parse-int (read-line))]
+            (cond 
+                 (= readinput 1) (do (println "i1") (recur) )
+                 (= readinput 2) (do (println "i2") (recur) )
+                 (= readinput 3) (do (println "i3") (recur) )
+                 (= readinput 4) (do (println "i4") (recur) )
+                 (= readinput 5) (do (println "i5") (recur) )
+                 (= readinput 6) (do (println "Good Bye") )
+                 (>= readinput 7) (do (println "Invalid option entered. Enter valid option.") (recur) )
+                 (<= readinput 0) (do (println "Invalid option entered. Enter valid option.") (recur) )
+            )
       )
+)
 
-(func)
-
-
-
-
-; (defn func  // trash
-;   []
-;       (println "wowo")
-;       (let [readinput (read-line)]
-;            (if (or (= readinput "1") (= readinput "2"))
-;              (do (println "i1") (println "i2"))
-;              (do (println "wrong") (if (or (= readinput "4") (= readinput "5"))
-;              (do (println "i1") (println "i2"))
-;              (do (println "wrong") )
-;              ))
-;              )
-
-                 
-;       )
-
-;       (println "func calling")
-;       (func)
-;       )
-
-; (func)
-
-
-
-
-; (defn func // gem
-;   []
-;       (println "wowo")
-;       (let [readinput (read-line)]
-;            (if (= readinput "1")
-;              (println "i1")
-;              (println "Wrong")))
-
-;       (println "func calling")
-;       (func)
-;       )
-
-; (func)
+(takeinput)
