955637a8d9f23993e614611c541e789bae325776
koshaapatel <koshapatel1045@gmail.com>
2020-06-04 15:42:36 -0400

    seperation

--- fileread.clj
+++ fileread.clj
@@ -1,10 +1,35 @@
-(use 'clojure.java.io)
+(require '[clojure.string :as str])
 
+(def custlist [])
+(def custvector {})
+(def templist[])
 
-(with-open [rdr (reader "cust.txt")]
-  (doseq [line (line-seq rdr)]
-    (println line)))
+(defn sep
+    [dataa]
+    (if (empty? dataa)
+        (println "nothing")
+        (do (println (str/join dataa))
+        (println (str/split (str/join dataa) #"|"))
+        )
+    ) 
+)
+(defn seperate
+    [data]
+    (if (empty? data)
+        (println "nothing")
+        (do (println (first data))
+        (sep (str/split (str/join (first data)) #"|"))
+        (recur (rest data)))
+    )     
+)
+(println "result:" (seperate (str/split (slurp "cust.txt") #"\n")))
 
-; (println (slurp "cust.txt"))
-
-
+; (defn sep
+;     [dataa]
+;     (if (empty? dataa)
+;         (println "nothing")
+;         (do (println (str/join dataa))
+;         (println (str/split (str/join (first dataa)) #"|"))
+;         )
+;     ) 
+; )
