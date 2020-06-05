35459f43cdb14616866a79808e7489566d7ae976
koshaapatel <koshapatel1045@gmail.com>
2020-06-04 21:48:07 -0400

    1st problem is solved, integrate it into first.clj

--- fileread.clj
+++ fileread.clj
@@ -2,35 +2,37 @@
 
 (def custlist [])
 (def custvector {})
-(def templist[])
+(def templist [])
+
+(defn parse-int [s]
+   (Integer. (re-find  #"\d+" s )))
 
 (defn seperate
     [data]
     (if (empty? data)
-        (println "nothing")
-        (do (println (first data))
-        ; (sep (str/split (str/join (first data)) #"|"))
-        (recur (rest data)))
-    )     
-)
-(println "result:" (seperate (str/split (slurp "cust.txt") #"\n")))
+        (do custvector)
+        (do 
+            (println (first data))
+            (def templist (str/split (first data) #"\|"))
+            (def custvector (conj custvector {(parse-int (get templist 0)) [(get templist 1) (get templist 2) (get templist 3)]} ))
+            ; (def custvector (conj custvector (assoc custvector (parse-int (get templist 0)) [(get templist 1) (get templist 2) (get templist 3)])))          
+            (recur (rest data))
+        )
+    )    
+) 
 
-; (defn sep
-;     [dataa]
-;     (if (empty? dataa)
-;         (println "nothing")
-;         (do (println (str/join dataa))
-;         (println (str/split (str/join (first dataa)) #"|"))
-;         )
-;     ) 
-; )
+; (println "result:" (seperate (str/split (slurp "cust.txt") #"\n")))
 
-; (defn sep
-;     [dataa]
-;     (if (empty? dataa)
-;         (do (println "nothing"))
-;         (do (println (str/join dataa))
-;         (println (str/split (str/join dataa) #"|"))
-;         )
-;     ) 
-; )
+(println (get custvector 2))
+
+(def xx (get custvector 1))
+(println (get xx 0))
+
+
+(println "SORTED MAP")
+(println (into (sorted-map) (seperate (str/split (slurp "cust.txt") #"\n"))))
+
+
+; (def custvector (conj custvector (assoc custvector :(parse-int (get templist 0)) [(get templist 1) (get templist 2) (get templist 3)])))
+
+; (println (get templist 0)) (sep (str/split (str/join (first data)) #"|"))
