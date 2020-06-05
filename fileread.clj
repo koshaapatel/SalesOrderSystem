951da29bd3ec60e220d93d5e031891127bba090d
koshaapatel <koshapatel1045@gmail.com>
2020-06-05 00:02:42 -0400

    theree problems are solved, integrate it with first.clj

--- fileread.clj
+++ fileread.clj
@@ -2,12 +2,21 @@
 
 (def custlist [])
 (def custvector {})
-(def templist [])
+(def prodlist [])
+(def prodvector {})
+(def saleslist [])
+(def salesvector {})
 
 (defn parse-int [s]
    (Integer. (re-find  #"\d+" s )))
 
+(defn parse-float [s]
+    (Float/parseFloat s))
+
 (defn splitcustdata
+    ([]
+    (do (into (sorted-map) custvector)))   
+
     ([data]
     (if (empty? data)
         (do custvector)
@@ -15,34 +24,66 @@
             (println (first data))
             (def custlist (str/split (first data) #"\|"))
             (def custvector (conj custvector {(parse-int (get custlist 0)) [(get custlist 1) (get custlist 2) (get custlist 3)]} ))
-            ; (def custvector (conj custvector (assoc custvector (parse-int (get custlist 0)) [(get custlist 1) (get custlist 2) (get custlist 3)])))          
+            ; (def custvector (conj custvector (assoc custvector (parse-int (get custlist 0)) [(get custlist 1) (get custlist 2) (get custlist 3)]))) ; alternate of above line, we wont use though       
             (recur (rest data))
         )
     ))
 
+)
+
+(defn splitproddata
     ([]
-    (do (into (sorted-map) custvector)))    
-) 
+    (do (into (sorted-map) prodvector)))   
 
-; (defn getmap
-;     []
-;     (splitcustdata (str/split (slurp "cust.txt") #"\n"))
-; )
+    ([data]
+    (if (empty? data)
+        (do prodvector)
+        (do 
+            (println (first data))
+            (def prodlist (str/split (first data) #"\|"))
+            (def prodvector (conj prodvector {(parse-int (get prodlist 0)) [(get prodlist 1) (parse-float (get prodlist 2))]} ))
+            (recur (rest data))
+        )
+    ))
 
-(println (get custvector 2))
+)
 
-(def xx (get custvector 1))
-(println (get xx 0))
+(defn splitsalesdata
+    ([]
+    (do (into (sorted-map) salesvector)))   
 
+    ([data]
+    (if (empty? data)
+        (do salesvector)
+        (do 
+            (println (first data))
+            (def saleslist (str/split (first data) #"\|"))
+            (def tempcustname (get custvector (parse-int (get saleslist 1)))) ; (println (get tempcustname 0))
+            (def tempitemname (get prodvector (parse-int (get saleslist 2)))) ; (println (get tempitemname 0))
+            (def salesvector (conj salesvector {(parse-int (get saleslist 0)) [(get tempcustname 0) (get tempitemname 0) (parse-int (get saleslist 3)) ]} ))
+            (recur (rest data))
+        )
+    ))
 
-(println "SORTED MAP")
+)
+
+(println "SORTED CUST MAP")
 (println (into (sorted-map) (splitcustdata (str/split (slurp "cust.txt") #"\n"))))
-
-;(println "result:" (splitcustdata (str/split (slurp "cust.txt") #"\n")))
-(println "MAPp")
+(println "CUST MAP")
 (println (splitcustdata))
 
+; additional operations on custvector
+; (println (get custvector 2))
+; (def xx (get custvector 1))
+; (println (get xx 0))
+; (println (get custlist 0)) (sep (str/split (str/join (first data)) #"|")) ; this might be invalid
 
-; (def custvector (conj custvector (assoc custvector :(parse-int (get custlist 0)) [(get custlist 1) (get custlist 2) (get custlist 3)])))
+(println "SORTED PROD MAP")
+(println (into (sorted-map) (splitproddata (str/split (slurp "prod.txt") #"\n"))))
+(println "PROD MAP")
+(println (splitproddata))
 
-; (println (get custlist 0)) (sep (str/split (str/join (first data)) #"|"))
+(println "SORTED SALES MAP")
+(println (into (sorted-map) (splitsalesdata (str/split (slurp "sales.txt") #"\n"))))
+(println "SALES MAP")
+(println (splitsalesdata))
