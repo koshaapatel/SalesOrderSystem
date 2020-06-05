209fbfa1d0554e7b8dd59234d213a7f05c5594a8
koshaapatel <koshapatel1045@gmail.com>
2020-06-05 00:15:50 -0400

    theree problems are solved, integrate it with first.clj

--- fileread.clj
+++ fileread.clj
@@ -15,13 +15,12 @@
 
 (defn splitcustdata
     ([]
-    (do (into (sorted-map) custvector)))   
+    (do (into (sorted-map) custvector) ))   
 
     ([data]
     (if (empty? data)
-        (do custvector)
-        (do 
-            (println (first data))
+        (do (into (sorted-map) custvector) )
+        (do ; (println (first data))
             (def custlist (str/split (first data) #"\|"))
             (def custvector (conj custvector {(parse-int (get custlist 0)) [(get custlist 1) (get custlist 2) (get custlist 3)]} ))
             ; (def custvector (conj custvector (assoc custvector (parse-int (get custlist 0)) [(get custlist 1) (get custlist 2) (get custlist 3)]))) ; alternate of above line, we wont use though       
@@ -33,13 +32,12 @@
 
 (defn splitproddata
     ([]
-    (do (into (sorted-map) prodvector)))   
+    (do (into (sorted-map) prodvector) ))   
 
     ([data]
     (if (empty? data)
-        (do prodvector)
-        (do 
-            (println (first data))
+        (do (into (sorted-map) prodvector) )
+        (do ; (println (first data))
             (def prodlist (str/split (first data) #"\|"))
             (def prodvector (conj prodvector {(parse-int (get prodlist 0)) [(get prodlist 1) (parse-float (get prodlist 2))]} ))
             (recur (rest data))
@@ -50,13 +48,12 @@
 
 (defn splitsalesdata
     ([]
-    (do (into (sorted-map) salesvector)))   
+    (do (into (sorted-map) salesvector) ))   
 
     ([data]
     (if (empty? data)
-        (do salesvector)
-        (do 
-            (println (first data))
+        (do (into (sorted-map) salesvector) )
+        (do ; (println (first data))
             (def saleslist (str/split (first data) #"\|"))
             (def tempcustname (get custvector (parse-int (get saleslist 1)))) ; (println (get tempcustname 0))
             (def tempitemname (get prodvector (parse-int (get saleslist 2)))) ; (println (get tempitemname 0))
@@ -68,7 +65,7 @@
 )
 
 (println "SORTED CUST MAP")
-(println (into (sorted-map) (splitcustdata (str/split (slurp "cust.txt") #"\n"))))
+(println (splitcustdata (str/split (slurp "cust.txt") #"\n")) )
 (println "CUST MAP")
 (println (splitcustdata))
 
@@ -79,11 +76,11 @@
 ; (println (get custlist 0)) (sep (str/split (str/join (first data)) #"|")) ; this might be invalid
 
 (println "SORTED PROD MAP")
-(println (into (sorted-map) (splitproddata (str/split (slurp "prod.txt") #"\n"))))
+(println (splitproddata (str/split (slurp "prod.txt") #"\n")) )
 (println "PROD MAP")
 (println (splitproddata))
 
 (println "SORTED SALES MAP")
-(println (into (sorted-map) (splitsalesdata (str/split (slurp "sales.txt") #"\n"))))
+(println (splitsalesdata (str/split (slurp "sales.txt") #"\n")) )
 (println "SALES MAP")
 (println (splitsalesdata))
